1c50efa500f88bd64c60a37c87d45384af25abb8
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2024-01-28 00:24:33 +0100

    Make fonts ajustable

--- CYD-Klipper/src/ui/nav_buttons.cpp
+++ CYD-Klipper/src/ui/nav_buttons.cpp
@@ -186,5 +186,5 @@ void nav_style_setup(){
     lv_style_set_radius(&nav_button_style, 0);
 
     lv_style_init(&nav_button_text_style);
-    lv_style_set_text_font(&nav_button_text_style, &lv_font_montserrat_10);
+    lv_style_set_text_font(&nav_button_text_style, CYD_SCREEN_FONT_SMALL);
 }
