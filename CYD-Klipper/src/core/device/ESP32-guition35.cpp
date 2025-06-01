22f22887e8f7403a870ccd21785328f998e4e119
Dylan Pryke-Watanabe <dylakaya@live.com>
2025-06-01 20:36:11 +1000

    Fix Display Flushing and Touch Screen works now!

--- CYD-Klipper/src/core/device/ESP32-guition35.cpp
+++ CYD-Klipper/src/core/device/ESP32-guition35.cpp
@@ -7,13 +7,61 @@
 #include "lvgl.h"
 #include "../lv_setup.h"
 #include "../../conf/global_config.h"
+#include <Wire.h>
 
 #define CPU_FREQ_HIGH 240
 #define CPU_FREQ_LOW 80
 
-#define CANVAS_WIDTH CYD_SCREEN_WIDTH_PX
-#define CANVAS_HEIGHT CYD_SCREEN_HEIGHT_PX
+struct TouchPoint {
+  uint8_t gesture;
+  uint8_t num;
+  uint8_t x_h : 4;
+  uint8_t _pad1 : 2;
+  uint8_t event : 2;
+  uint8_t x_l;
+  uint8_t y_h : 4;
+  uint8_t _pad2 : 4;
+  uint8_t y_l;
+} __attribute__((packed));
 
+// Reads 1 touch point from AXS15231B controller at I2C addr 0x3B
+bool read_touch(uint16_t &x, uint16_t &y) {
+
+    Wire.beginTransmission(0x3B);
+    uint8_t error = Wire.endTransmission();
+    Serial.printf("I2C test to 0x3B returned: %d\n", error);
+    
+  const uint8_t addr = 0x3B; // confirmed via scanner
+  const uint8_t read_cmd[11] = {
+    0xB5, 0xAB, 0xA5, 0x5A, 0x00, 0x00,
+    0x00, 8, // length of expected reply (MSB, LSB)
+    0x00, 0x00, 0x00
+  };
+
+  Wire.beginTransmission(addr);
+  Wire.write(read_cmd, sizeof(read_cmd));
+  if (Wire.endTransmission(false) != 0) {
+    return false;
+  }
+
+  uint8_t data[8] = {0};
+  if (Wire.requestFrom(addr, (uint8_t)sizeof(data)) != sizeof(data)) {
+    return false;
+  }
+
+  for (uint8_t i = 0; i < sizeof(data); i++) {
+    data[i] = Wire.read();
+  }
+
+  TouchPoint *p = (TouchPoint *)data;
+  if (p->num > 0 && p->num <= 2) {
+    x = ((p->x_h & 0x0F) << 8) | p->x_l;
+    y = ((p->y_h & 0x0F) << 8) | p->y_l;
+    return true;
+  }
+
+  return false;
+}
 
 static Arduino_ESP32QSPI qspiBus(
     LCD_CS, LCD_CLK,
@@ -25,7 +73,7 @@ Arduino_GFX *gfx = new Arduino_AXS15231B(
     &qspiBus, -1, 0, true,
     LCD_WIDTH, LCD_HEIGHT,
     0, 0, 0, 0);
-static Arduino_Canvas canvas(CANVAS_WIDTH, CANVAS_HEIGHT, gfx);
+static Arduino_Canvas canvas(CYD_SCREEN_WIDTH_PX, CYD_SCREEN_HEIGHT_PX, gfx, 0, 0);
 
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t *buf = nullptr;
@@ -38,16 +86,28 @@ void screen_setBrightness(uint8_t brightness)
 
 void screen_lv_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
-    // Full frame flush each time
-    canvas.draw16bitRGBBitmap(0, 0, (uint16_t *)color_p, CANVAS_WIDTH, CANVAS_HEIGHT);
+    int x = area->x1;
+    int y = area->y1;
+    int w = area->x2 - area->x1 + 1;
+    int h = area->y2 - area->y1 + 1;
+    canvas.draw16bitRGBBitmap(x, y, (uint16_t *)color_p, w, h);
     canvas.flush();
     lv_disp_flush_ready(disp);
 }
 
 void screen_lv_touchRead(lv_indev_drv_t * /*indev_driver*/, lv_indev_data_t *data)
 {
-    data->state = LV_INDEV_STATE_REL;  // No touch support
+  uint16_t x, y;
+  if (read_touch(x, y)) {
+    data->state = LV_INDEV_STATE_PR;
+    data->point.x = x;
+    data->point.y = y;
+    Serial.printf("Touch: x=%d, y=%d\n", x, y);
+  } else {
+    data->state = LV_INDEV_STATE_REL;
+  }
 }
+
 
 void set_invert_display()
 {
@@ -56,6 +116,7 @@ void set_invert_display()
 
 void screen_setup()
 {
+    Serial.begin(115200);
     pinMode(LCD_BL_PIN, OUTPUT);
     ledcSetup(0, 5000, 12);
     ledcAttachPin(LCD_BL_PIN, 0);
@@ -67,21 +128,23 @@ void screen_setup()
     canvas.fillScreen(0x0000);
     canvas.flush();
 
+    Wire.begin(TOUCH_SDA, TOUCH_SCL);
+
     lv_init();
 
     // Allocate full canvas buffer for LVGL
     buf = (lv_color_t *)heap_caps_malloc(
-        CANVAS_WIDTH * CANVAS_HEIGHT * sizeof(lv_color_t),
+        CYD_SCREEN_WIDTH_PX * CYD_SCREEN_HEIGHT_PX * sizeof(lv_color_t),
         MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
 
     assert(buf);
 
-    lv_disp_draw_buf_init(&draw_buf, buf, NULL, CANVAS_WIDTH * CANVAS_HEIGHT);
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, CYD_SCREEN_WIDTH_PX * CYD_SCREEN_HEIGHT_PX);
 
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
-    disp_drv.hor_res = CANVAS_WIDTH;
-    disp_drv.ver_res = CANVAS_HEIGHT;
+    disp_drv.hor_res = CYD_SCREEN_WIDTH_PX;
+    disp_drv.ver_res = CYD_SCREEN_HEIGHT_PX;
     disp_drv.flush_cb = screen_lv_flush;
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register(&disp_drv);
